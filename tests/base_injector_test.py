8baf4dc1b162d43cd084f22f4c8e11d33bffc717
AsfhtgkDavid <daika@i.ua>
2025-03-17 12:48:57 +0200

    refactor: refactor BaseInjector tests

--- tests/base_injector_test.py
+++ tests/base_injector_test.py
@@ -30,18 +30,17 @@ class BaseInjectorTest(unittest.TestCase):
         dep = random.randint(0, 10)
         easy_di.BaseInjector.register('test', dep)
         self.assertTupleEqual((x, dep), func(x))
-        easy_di.BaseInjector.unregister('test')
 
     def test_inject_and_pass_deps(self) -> None:
         func = easy_di.BaseInjector('test')(mock_func)
         x = random.randint(0, 10)
         with self.assertRaises(OverwritingArgumentError):
-            func(x, deps=x)
+            func(x, deps=x)  # type: ignore
 
     def test_decorate_when_dependency_not_registered(self) -> None:
         func = easy_di.BaseInjector('test')(mock_func)
         with self.assertRaises(DependencyNotRegisteredError) as e:
-            func()
+            func(1)
         self.assertEqual(e.exception.dependency_id, 'test')
 
     def test_decorate_when_dependency_type_incorrect(self) -> None:
@@ -53,11 +52,10 @@ class BaseInjectorTest(unittest.TestCase):
         with self.assertRaises(DependencyRegisteredError) as e:
             easy_di.BaseInjector.register('test', 'test')
         self.assertEqual(e.exception.dependency_id, 'test')
-        easy_di.BaseInjector.unregister('test')
 
     def test_register_incorrect_dependency_id(self) -> None:
         with self.assertRaises(TypeError) as e:
-            easy_di.BaseInjector.register(123, 'test')
+            easy_di.BaseInjector.register(123, 'test')  # type: ignore
 
     def test_unregister_when_dependency_not_registered(self) -> None:
         with self.assertRaises(DependencyNotRegisteredError) as e:
